1ff9c0976353d671633c08a35d2b7b780d1ce466
dahhyeon <dahhyeon22@gmail.com>
2025-03-23 00:51:17 +0900

    chore: 절대경로 alias 설정

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  return <div>react 초기 세팅</div>;
+  return <div></div>;
 }
 
 export default App;
---
+++ blog/src/routes/route.tsx
@@ -0,0 +1,4 @@
+import { createBrowserRouter } from 'react-router-dom';
+import HomePage from '@/pages/home';
+
+const router = createBrowserRouter([]);
