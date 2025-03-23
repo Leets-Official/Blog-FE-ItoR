a18ddb398201a45d42ac20fb744a4798545ceb46
nabbang6 <hawwah03@gachon.ac.kr>
2025-03-24 02:24:15 +0900

    feat: vite, prettier & eslint 설정 >> - .prettierrc 파일을 생성하고 설정 추가 >> - eslint.config.js 설정 >> - ESLint & Prettier 충돌 방지 설정 및 플러그인 설치 >> - 절대 경로 참조하도록  jsconfig.json 파일 추가, vite.config.js 설정 추가

--- 서나영-Blog/eslint.config.js
+++ 서나영-Blog/eslint.config.js
@@ -1,7 +1,10 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-config-prettier';  // Prettier 설정 추가
+import prettierPlugin from 'eslint-plugin-prettier';  // Prettier 플러그인 추가
 
 export default [
   { ignores: ['dist'] },
@@ -17,17 +20,23 @@ export default [
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,  // React 추천 규칙 적용
+      ...react.configs['jsx-runtime'].rules, // JSX 런타임 관련 규칙
+      'react/jsx-no-target-blank': 'off',
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 'error',  // Prettier 규칙을 위반하면 ESLint에서 에러로 처리
     },
   },
 ]
