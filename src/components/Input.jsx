ac9ecd211ff66496e2ed295e30e0248e53594cc7
정찬원 <jcw5522@naver.com>
2025-03-24 19:52:36 +0900

    feat: Input 컴포넌트 추가

---
+++ src/components/Input.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledInput = styled.input`
+  width: ${(props) => props.width || "200px"};
+  height: ${(props) => props.height || "40px"};
+  font-size: ${props => props.fontSize || '16px'};
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const Input = ({ width, height, fontSize }) => {
+  return <StyledInput width={width} height={height} fontSize ={fontSize} />
+};
+
+export default Input;
