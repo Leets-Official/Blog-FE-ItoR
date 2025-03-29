bb98e247593ccab41ec923cd879a09d95c51b2e3
정찬원 <jcw0522@gachon.ac.kr>
2025-03-29 16:02:16 +0900

    chore: Toast 컴포넌트 색상 수정

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -5,13 +5,12 @@ import DoneIcon from '@/assets/done.svg?react';
 
 const ToastMessage = styled.div`
   position: absolute;
-  bottom: 50px;
   left: 50%;
   transform: translateX(-50%);
   padding: 8px 12px;
   background: white;
-  color: ${(props) => (props.type === 'positive' ? '#00A1FF' : '#FF3F3F')};
-  border: 1px solid ${(props) => (props.type === 'positive' ? '#00A1FF' : '#FF3F3F')};
+  color: ${(props) => (props.type === 'positive' ? '#15DC5E' : '#FF3F3F')};
+  border: 1px solid ${(props) => (props.type === 'positive' ? '#15DC5E' : '#FF3F3F')};
   border-radius: 25px;
   opacity: ${(props) => (props.$show ? '1' : '0')};
   transition: opacity 0.5s;
