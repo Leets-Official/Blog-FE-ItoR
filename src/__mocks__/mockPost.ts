d3ff81ebe392d04faaf6d29058a4c56fb4818201
dahhyeon <dahhyeon22@gmail.com>
2025-03-30 22:48:10 +0900

    feat: faker-js를 사용하여 목데이터를 렌더링하도록 추가

---
+++ src/__mocks__/mockPost.ts
@@ -0,0 +1,17 @@
+import { Post } from '@/types/post';
+import { faker } from '@faker-js/faker';
+
+export const mockPosts: Post[] = Array.from({ length: 10 }, (_, i) => ({
+  id: i + 1,
+  title: faker.lorem.sentence(),
+  content: faker.lorem.paragraph(),
+  nickName: faker.internet.username(),
+  createAt: faker.date.recent().toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  }),
+  image: faker.image.urlPicsumPhotos(),
+  commentCount: faker.number.int({ min: 0, max: 20 }),
+  profileImage: faker.image.avatar(),
+}));
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,12 @@
 import Header from '@/components/common/Header/Header';
 import PostList from '@/components/home/PostList';
+import { mockPosts } from '@/__mocks__/mockPost';
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <Header variant="write" />
-      <PostList />
+      <PostList posts={mockPosts} />
     </div>
   );
 };
