86540d7a783f7883a08f753dfd5349c93f2929e0
nabbang6 <hawwah03@gachon.ac.kr>
2025-04-01 03:53:19 +0900

    refactor: 기존 css 제거 & global.ts로 적용

---
+++ 서나영-Blog/src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+import AppleSDGothicNeoM from '@/fonts/AppleSDGothicNeoM.ttf';
+import NotoSansKRMedium from '@/fonts/NotoSansKR-Medium.ttf';
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Noto Sans';
+    src: url(${NotoSansKRMedium}) format('truetype');
+}
+
+  @font-face {
+    font-family: 'AppleSDGothicNeoM';
+    src: url(${AppleSDGothicNeoM}) format('truetype');
+}
+
+body {
+  background-color: #FFF;
+  font-family: 'Noto Sans', sans-serif;;
+}
+
+* {
+  box-sizing: border-box;
+}
+`;
+
+export default GlobalStyle;
