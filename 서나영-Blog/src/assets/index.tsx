a0e9f63353350b504d3c9da5e6b10125ebcc1a8e
nabbang6 <hawwah03@gachon.ac.kr>
2025-04-01 03:49:04 +0900

    feat: svg 파일 추가 및 assets/icons로 이동, index.tsx 추가

---
+++ 서나영-Blog/src/assets/index.tsx
@@ -0,0 +1,41 @@
+import AddPhoto from '@/assets/icons/add_photo_alternate.svg';
+import Chat from '@/assets/icons/chat.svg';
+import Clear from '@/assets/icons/clear.svg';
+import Create from '@/assets/icons/create.svg';
+import Delete from '@/assets/icons/delete_forever.svg';
+import Divider from '@/assets/icons/divider.svg';
+import Done from '@/assets/icons/done.svg';
+import ErrorOutline from '@/assets/icons/error_outline.svg';
+import FolderOpen from '@/assets/icons/folder_open.svg';
+import GITLOG from '@/assets/icons/GITLOG.svg';
+import Kakao from '@/assets/icons/kakao.svg';
+import Left from '@/assets/icons/Left.svg';
+import MoreVert from '@/assets/icons/more_vert.svg';
+import NavigateBefore from '@/assets/icons/navigate_before.svg';
+import Plus from '@/assets/icons/plus.svg';
+import Reorder from '@/assets/icons/reorder.svg';
+import Right from '@/assets/icons/right.svg';
+import Settings from '@/assets/icons/settings.svg';
+
+const icons = {
+  AddPhoto,
+  Chat,
+  Clear,
+  Create,
+  Delete,
+  Divider,
+  Done,
+  ErrorOutline,
+  FolderOpen,
+  GITLOG,
+  Kakao,
+  Left,
+  MoreVert,
+  NavigateBefore,
+  Plus,
+  Reorder,
+  Right,
+  Settings,
+};
+
+export default icons;
