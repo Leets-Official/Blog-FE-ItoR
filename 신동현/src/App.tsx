9c4f2429a8f0e64cd2f20bfca433306a62bdc7ca
Donghyeon Shin <shindong0321@gmail.com>
2025-04-01 18:11:35 +0900

    feat : page index로 한번에 불러오기

--- 신동현/src/App.tsx
+++ 신동현/src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
-import SignUpDetail from "./pages/SignUpDetail";
+import { Home, SignUp, SignUpDetail } from "./pages";
 
 function App() {
 
