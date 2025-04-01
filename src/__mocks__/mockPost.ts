8e2cd79d640623aa173e9efa336cf09cb48e7c8f
dahhyeon <dahhyeon22@gmail.com>
2025-04-01 10:38:39 +0900

    refactor: 랜덤 시간 차를 생성하여 날짜 형식 확인

--- src/__mocks__/mockPost.ts
+++ src/__mocks__/mockPost.ts
@@ -4,27 +4,15 @@ import { faker } from '@faker-js/faker';
 export const mockPosts: Post[] = Array.from({ length: 30 }, (_, i) => {
   const now = new Date();
 
-  const createAt = (() => {
-    // test
-    if (i < 5) {
-      // 하루 이상 지난 날짜 (2일전)
-      const pastDate = new Date(now);
-      pastDate.setDate(pastDate.getDate() - 2);
-      return pastDate.toISOString();
-    } else {
-      // 하루 이하 ( 13시간 전)
-      const recentDate = new Date(now);
-      recentDate.setHours(recentDate.getHours() - 13);
-      return recentDate.toISOString();
-    }
-  })();
+  const randomMinutesAgo = faker.number.int({ min: 0, max: 7200 });
+  const createdAt = new Date(now.getTime() - randomMinutesAgo * 60 * 1000).toISOString();
 
   return {
     id: i + 1,
     title: faker.lorem.words(),
     content: Math.random() < 0.5 ? faker.lorem.paragraph() : faker.lorem.paragraphs(),
     nickName: faker.internet.username(),
-    createAt,
+    createAt: createdAt,
     image:
       Math.random() < 0.5 ? `https://picsum.photos/seed/${faker.string.uuid()}/220/220` : undefined,
     commentCount: faker.number.int({ min: 0, max: 20 }),
