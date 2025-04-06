86ed14a8926cc840a0f54d6970fae0ab64b7c72d
nabbang6 <hawwah03@gachon.ac.kr>
2025-04-07 01:22:04 +0900

    style: 로그인 Schema 에러 메시지 수정

--- 서나영-Blog/src/schema/auth.ts
+++ 서나영-Blog/src/schema/auth.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string().email({ message: '이메일 형식이 적합하지 않습니다.' }),
-  password: z.string().min(6, { message: '비밀번호는 최소 6자 이상이어야 합니다.' }),
+  email: z.string().email({ message: '* 이메일 형식이 적합하지 않습니다.' }),
+  password: z.string().min(6, { message: '* 비밀번호는 최소 6자 이상이어야 합니다.' }),
 });
 
 export const signupSchema = z
