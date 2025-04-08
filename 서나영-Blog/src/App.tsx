855e98d4fc39626d04ef2dcbca3a18d2d71826c7
nabbang6 <hawwah03@gachon.ac.kr>
2025-04-08 17:42:12 +0900

    feat: 블로그 작성 페이지 UI 구현

--- 서나영-Blog/src/App.tsx
+++ 서나영-Blog/src/App.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastProvider } from './components/ui/Toast';
 import Signup from '@/pages/signup/Signup';
 import SignupSelect from '@/pages/signup/SignupSelect';
-import BlogDetail from './pages/BlogDetail';
+import BlogDetail from '@/pages/BlogDetail';
 import Home from '@/pages/Home';
+import MyPage from '@/pages/mypage/MyPage';
+import MyPageSetting from '@/pages/mypage/MyPageSetting';
+import BlogEditor from '@/pages/BlogEditor';
 
 function App() {
   return (
@@ -13,9 +16,12 @@ function App() {
       <ToastProvider>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/blog/editor' element={<BlogEditor />} />
           <Route path='/blog/:postId' element={<BlogDetail />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/signup/select' element={<SignupSelect />} />
+          <Route path='/mypage' element={<MyPage />} />
+          <Route path='/mypage/setting' element={<MyPageSetting />} />
         </Routes>
       </ToastProvider>
     </BrowserRouter>
---
+++ 서나영-Blog/src/components/editor/TitleField.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+const TitleFieldContainer = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 12px 16px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 8px;
+  align-self: stretch;
+  margin-bottom: 32px;
+  margin-top: 203px;
+  border-bottom: 1px solid #f5f5f5;
+`;
+
+const TextBox = styled.input`
+  display: flex;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 688px;
+  padding: 12px 16px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 8px;
+  align-self: stretch;
+  font-family: 'Noto Sans M';
+  font-size: 16px;
+  font-weight: '500';
+  letter-spacing: -0.04px;
+  border: none;
+  padding: 12px 16px;
+
+  &::placeholder {
+    color: #ccc;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: none;
+  }
+`;
+
+interface TitleFieldProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+}
+
+const TitleField = ({ value, onChange, placeholder = '제목' }: TitleFieldProps) => {
+  return (
+    <TitleFieldContainer>
+      <TextBox value={value} onChange={onChange} placeholder={placeholder} />
+    </TitleFieldContainer>
+  );
+};
+
+export default TitleField;
