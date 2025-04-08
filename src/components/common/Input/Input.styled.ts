441ed53562a4393d21688dbdce73ebc405ca714b
dahhyeon <dahhyeon22@gmail.com>
2025-04-08 23:44:30 +0900

    chore: 필요없는 속성 제거

--- src/components/common/Input/Input.styled.ts
+++ src/components/common/Input/Input.styled.ts
@@ -45,8 +45,7 @@ export const StyledInput = styled.input<{
   color: ${({ textColor, readOnly, theme }) =>
     textColor ?? (readOnly ? theme.COLORS.gray[56] : theme.COLORS.black)};
 
-  border: ${({ theme, readOnly, showBorder }) =>
-    !showBorder || readOnly ? 'none' : `1px solid ${theme.COLORS.gray[78]}`};
+  border: ${({ theme, readOnly }) => (readOnly ? 'none' : `1px solid ${theme.COLORS.gray[78]}`)};
 
   &:focus {
     outline: none;
