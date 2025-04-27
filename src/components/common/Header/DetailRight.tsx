322d8794b1b4c927eb483a05b325659cc0eaca96
dahhyeon <dahhyeon22@gmail.com>
2025-04-27 17:52:02 +0900

    feat:상세 게시물 수정/삭제 모달 추가

--- src/components/common/Header/DetailRight.tsx
+++ src/components/common/Header/DetailRight.tsx
@@ -1,11 +1,26 @@
 import { CommentSvg, MeatballSvg } from '@/assets';
 import { SectionWrapper } from '@/components/common/Header/Header.styled';
+import { useState } from 'react';
+import DetailModal from '../Modal/DetailModal';
 
 const DetailRight: React.FC<{ onClick?: (action: string) => void }> = ({ onClick }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
+  const handleAction = (action: 'edit' | 'delete') => {
+    onClick?.(action);
+    setIsModalOpen(false);
+  };
+
   return (
     <SectionWrapper>
       <CommentSvg onClick={() => onClick?.('comment')} />
-      <MeatballSvg onClick={() => onClick?.('meatball')} />
+      <MeatballSvg onClick={handleModalOpen} />
+
+      {isModalOpen && <DetailModal onClose={() => setIsModalOpen(false)} onAction={handleAction} />}
     </SectionWrapper>
   );
 };
---
+++ src/components/common/Modal/DetailModal.tsx
@@ -0,0 +1,70 @@
+import { flexColumn } from '@/styles/common.styled';
+import { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+interface DetailModalProps {
+  onClose: () => void;
+  onAction: (action: 'edit' | 'delete') => void;
+}
+export const ModalWrapper = styled.div`
+  ${flexColumn}
+  width:160px;
+  position: absolute;
+  top: 70px;
+  right: 20px;
+  background-color: ${({ theme }) => theme.COLORS.white};
+  box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+  overflow: visible;
+  border-radius: 4px;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: -7px;
+    right: 12px;
+    width: 15px;
+    height: 15px;
+    transform: rotate(45deg);
+    box-shadow: 0px -2px 2px rgba(0, 0, 0, 0.05);
+    z-index: 1;
+    background-color: ${({ theme }) => theme.COLORS.white};
+  }
+`;
+
+export const MenuItems = styled.div<{ isNegative?: boolean }>`
+  padding: 18px 16px;
+  text-align: left;
+  cursor: pointer;
+  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT.regular};
+  color: ${({ theme, isNegative }) => (isNegative ? theme.COLORS.negative : theme.COLORS.black)};
+`;
+
+const DetailModal: React.FC<DetailModalProps> = ({ onClose, onAction }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
+  return (
+    <ModalWrapper ref={modalRef}>
+      <MenuItems onClick={() => onAction('edit')}>수정하기</MenuItems>
+      <MenuItems isNegative onClick={() => onAction('delete')}>
+        삭제하기
+      </MenuItems>
+    </ModalWrapper>
+  );
+};
+
+export default DetailModal;
