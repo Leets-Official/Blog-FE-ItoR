171d56a0948d2af3e487c1713692b2ee90e776cb
nabbang6 <hawwah03@gachon.ac.kr>
2025-04-29 16:12:24 +0900

    feat: presigned URL 요청 및 이미지 업로드 API 구현

---
+++ 서나영-Blog/src/api/ImageAPI.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import api from '@/api/axios';
+
+/**
+ * presigned URL 요청
+ */
+export const getPresignedUrl = async (fileName: string): Promise<string> => {
+  const response = await api.get('/images/presigned-url', {
+    params: { fileName },
+  });
+  console.log('Image url 요청 응답:', response.data);
+  return response.data.data;
+};
+
+/**
+ * presigned URL로 이미지 업로드
+ */
+export const uploadImage = async (presignedUrl: string, file: File): Promise<string> => {
+  await axios.put(presignedUrl, file, {
+    headers: {
+      'Content-Type': file.type,
+    },
+  });
+
+  return presignedUrl.split('?')[0];
+};
