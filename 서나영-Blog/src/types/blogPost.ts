49e0f3636ec4c6929351e022264a1d5d53845cc2
nabbang6 <hawwah03@gachon.ac.kr>
2025-05-06 01:30:32 +0900

    feat: BlogPost, BlogPostDetail, BlogComment, Block 타입 정의

--- 서나영-Blog/src/types/blogPost.ts
+++ 서나영-Blog/src/types/blogPost.ts
@@ -1,18 +1,34 @@
-export interface BlogPost {
-  postId: string;
-  title: string;
-  contents: BlogPostContent[];
-  comments: BlogComment[];
-  nickName: string;
-  createdAt: string;
-  profileUrl: string;
-  isOwner: boolean;
-}
-
 export interface BlogPostContent {
   contentOrder: number;
   content: string;
   contentType: 'TEXT' | 'IMAGE';
+}
+export interface BlogPost {
+  postId: string;
+  title: string;
+  contents: BlogPostContent[];
+  isOwner: boolean;
+  commentCount?: number;
+}
+
+export interface BlogPostDetail {
+  postId: string;
+  title: string;
+  contents: {
+    contentOrder: number;
+    content: string;
+    contentType: 'TEXT' | 'IMAGE';
+  }[];
+  isOwner: boolean;
+  comments: {
+    commentId: number;
+    content: string;
+    nickName: string;
+    isOwner: boolean;
+  }[];
+  nickName: string;
+  profileUrl: string;
+  createdAt: string;
 }
 
 export interface BlogComment {
@@ -20,4 +36,9 @@ export interface BlogComment {
   content: string;
   nickName: string;
   isOwner: boolean;
+}
+
+export interface Block {
+  content: string;
+  type: 'TEXT' | 'IMAGE';
 }
