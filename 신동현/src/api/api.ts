8747fef8560a211b1931ff0e62099a298fd2f1ed
Donghyeon Shin <shindong0321@gmail.com>
2025-05-05 18:25:22 +0900

    feat : Access Token 재발급 함수 및 api 인터셉트 구현

--- 신동현/src/api/api.ts
+++ 신동현/src/api/api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosHeaders, InternalAxiosRequestConfig } from "axios";
 
 const BaseUrl = import.meta.env.VITE_API_URL;
 
@@ -9,7 +9,85 @@ const api = axios.create({
   },
 });
 
-export { BaseUrl,api };
+const getAccessToken = async () => {
+  const refreshToken = localStorage.getItem("refreshToken");
+  if (!refreshToken) {
+    return;
+  }
+  try {
+    const response = await api.post("/auth/reissue", {
+      refreshToken,
+    });
+
+    if (response.status === 200) {
+      const { accessToken, refreshToken } = response.data;
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+      return {
+        accessToken,
+        refreshToken,
+      };
+    }
+    throw new Error("Failed to refresh access token");
+  } catch (error) {
+    console.error("Failed to refresh access token", error);
+    throw error;
+  }
+};
+
+api.interceptors.request.use(async (config: InternalAxiosRequestConfig) => {
+  const accessToken = localStorage.getItem("accessToken");
+  const refreshToken = localStorage.getItem("refreshToken");
+
+  const newHeaders = new AxiosHeaders(config.headers);
+
+  if (accessToken) {
+    newHeaders.set("Authorization", `Bearer ${accessToken}`);
+  }
+
+  if (refreshToken) {
+    newHeaders.set("Refresh", `Bearer ${refreshToken}`);
+  }
+
+  return {
+    ...config,
+    headers: newHeaders,
+  };
+}, (error) => {
+  return Promise.reject(error);
+});
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const tokens = await getAccessToken();
+        if (!tokens) {
+          localStorage.removeItem('accessToken');
+          localStorage.removeItem('refreshToken');
+          window.location.href = '/';
+          return Promise.reject(error);
+        }
+        originalRequest.headers.Authorization = `Bearer ${tokens.accessToken}`;
+        originalRequest.headers.Refresh = `Bearer ${tokens.refreshToken}`;
+        return api(originalRequest);
+      } catch (error) {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        window.location.href = '/';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+export { BaseUrl, api };
 
 
 
