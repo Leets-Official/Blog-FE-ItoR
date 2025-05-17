ca8de072bbbcdc54933b0c51a043f15f4dd9e5d4
nabbang6 <hawwah03@gachon.ac.kr>
2025-05-18 04:14:11 +0900

    feat: react-query 설치 후 적용

--- 서나영-Blog/src/App.tsx
+++ 서나영-Blog/src/App.tsx
@@ -9,24 +9,31 @@ import MyPage from '@/pages/mypage/MyPage';
 import MyPageSetting from '@/pages/mypage/MyPageSetting';
 import BlogEditor from '@/pages/BlogEditor';
 import KakaoLoginProcess from './pages/signup/kakaoLoginProcess';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <ToastProvider>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/blog/editor' element={<BlogEditor />} />
-          <Route path='/blog/editor/:postId' element={<BlogEditor />} />
-          <Route path='/blog/:postId' element={<BlogDetail />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/signup/select' element={<SignupSelect />} />
-          <Route path='/oauth/kakao/success' element={<KakaoLoginProcess />} />
-          <Route path='/mypage' element={<MyPage />} />
-          <Route path='/mypage/setting' element={<MyPageSetting />} />
-        </Routes>
-      </ToastProvider>
+      <QueryClientProvider client={queryClient}>
+        <ToastProvider>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/blog/editor' element={<BlogEditor />} />
+            <Route path='/blog/editor/:postId' element={<BlogEditor />} />
+            <Route path='/blog/:postId' element={<BlogDetail />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/signup/select' element={<SignupSelect />} />
+            <Route path='/oauth/kakao/success' element={<KakaoLoginProcess />} />
+            <Route path='/mypage' element={<MyPage />} />
+            <Route path='/mypage/setting' element={<MyPageSetting />} />
+          </Routes>
+        </ToastProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
