6823ffe8a656bd932b211d7eab5c64e37ede28ba
nabbang6 <hawwah03@gachon.ac.kr>
2025-05-18 04:23:49 +0900

    feat: BlogPostListResponse 타입 추가

--- 서나영-Blog/src/types/blogPost.ts
+++ 서나영-Blog/src/types/blogPost.ts
@@ -9,23 +9,22 @@ export interface BlogPost {
   contents: BlogPostContent[];
   isOwner: boolean;
   commentCount?: number;
+  nickName: string;
+  profileUrl: string;
+  createdAt: string;
+}
+
+export interface BlogPostListResponse {
+  post: BlogPost[];
+  pageMax: number;
 }
 
 export interface BlogPostDetail {
   postId: string;
   title: string;
-  contents: {
-    contentOrder: number;
-    content: string;
-    contentType: 'TEXT' | 'IMAGE';
-  }[];
+  contents: BlogPostContent[];
   isOwner: boolean;
-  comments: {
-    commentId: number;
-    content: string;
-    nickName: string;
-    isOwner: boolean;
-  }[];
+  comments: BlogComment[];
   nickName: string;
   profileUrl: string;
   createdAt: string;
